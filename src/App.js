43545c49cc04d4bbbce7c25c1d1ee8fd7eecdfdb
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-10-19 18:49:53 -0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from "./components/login/Login";
 
 function App() {
   return (
-    <div className="App-header">
-
-      
+    <div >
+      <Login/>
     </div>
   );
 }
