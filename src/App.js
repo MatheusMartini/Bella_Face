1474ffcec6dc2ecdf412ef13ca26edc964b190f9
Matheus Martini <matheus.martini@rede.ulbra.br>
2020-10-20 10:20:24 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
-import Login from "./components/login/Login";
+import React from "react";
+import "./assets/styles/global.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Header from "./assets/components/Header";
+import Navbar from "./assets/components/Navbar";
+import Section from "./assets/components/Section";
+import Footer from "./assets/components/Footer";
 
 function App() {
   return (
-    <div >
-      <Login/>
-    </div>
+    <Container fluid>
+      <Router>
+        <Header />
+        <Navbar />
+        <Section />
+        <Footer />
+      </Router>
+    </Container>
   );
 }
 
