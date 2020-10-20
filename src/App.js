7f116a4a46a174faca0502fa4e0ab9228118026c
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-10-20 16:46:27 -0300

    remove header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import Header from "./assets/components/Header";
 import Navbar from "./assets/components/Navbar";
 import Section from "./assets/components/Section";
 import Footer from "./assets/components/Footer";
@@ -13,7 +12,6 @@ function App() {
   return (
     <Container fluid>
       <Router>
-        <Header />
         <Navbar />
         <Section />
         <Footer />
