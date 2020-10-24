af6f3733f8af33df4d7c4a887248c654a5fb9f31
Matheus Martini <matheus.martini@rede.ulbra.br>
2020-10-24 20:08:04 -0300

    alter navbar

--- src/assets/components/Navbar/index.js
+++ src/assets/components/Navbar/index.js
@@ -1,31 +1,22 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function CustomNavBar() {
   return (
-    <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/contacts">Contacts</Nav.Link>
-            <NavDropdown title="States" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/state/1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="/state/2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/state/3">Something</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="/contacts/view">View Contacts</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>Bella Face</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link>
+          <Link exact to="/home">
+            Home
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="/carrinho">Carrinho de compras</Link>
+        </Nav.Link>
+      </Nav>
+    </Navbar>
   );
 }
 
