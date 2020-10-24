2f445fb85b2a1dfe850a8846be857fbc68e63bc9
Matheus Martini <matheus.martini@rede.ulbra.br>
2020-10-24 20:07:15 -0300

    app.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import Navbar from "./assets/components/Navbar";
 import Section from "./assets/components/Section";
-import Footer from "./assets/components/Footer";
 
 function App() {
   return (
     <Container fluid>
-      <Router>
-        <Navbar />
-        <Section />
-        <Footer />
+      <Router> 
+        <Section/>
       </Router>
     </Container>
   );
