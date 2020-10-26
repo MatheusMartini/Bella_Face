5fd9a424a817eaec6d3d5ef75b084a5a0026c3e0
Matheus Martini <matheus.martini@rede.ulbra.br>
2020-10-26 08:14:58 -0300

    api

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState} from "react";
 import { Container, Row, Col, Card, Spinner, CardColumns, ListGroup, Button} from "react-bootstrap";
 
 import Navbar from "../../assets/components/Navbar";
+import api from "../../services/api";
 import AuthService from "../../services/auth.service";
 
 function Home() {
@@ -24,9 +25,25 @@ function Home() {
     AuthService.pedidos()
     .then(res =>{
       setPedidos(res.ordder)
+      setQtdPedidos(res.ordder.length)
     })
   }, pedidos)
+
+  useEffect(()=>{})
+
+  const inserirProduto = (order_order_id, product_id_product, quantity, unit_price) => {
+    return api.post("/pedidos/inpedido",{
+      order_order_id: order_order_id,
+      product_id_product: product_id_product,
+      quantity: quantity,
+      unit_price: unit_price,
+      total_price: quantity*unit_price
+      })
+      .then((response) => {});
+  };
   
+  
+
   return (
     <>
     <Navbar/>
@@ -35,7 +52,7 @@ function Home() {
         <Col sm={8}>
           <Row>
             {quantidade > 0 ? (
-              produtos.map((row, index) => {
+              produtos.map( row => {
                 return (
                   <Col sm={4} key={row.id_product}>
                     <CardColumns>
@@ -48,7 +65,7 @@ function Home() {
                           <Card.Text>
                             {row.description}
                           </Card.Text>
-                          <Button onClick={() =>{}}>Adicionar ao carrinho</Button>
+                          <Button onClick={(e)=>{inserirProduto(1, row.id_product, 2, row.price, e.preventDefault)}}>adicionar</Button>
                         </Card.Body>
                       </Card>
                     </CardColumns>      
@@ -69,19 +86,22 @@ function Home() {
         <Col Col sm={4}>
           <Row>
             <Col>
-              {qtdPedidos > 0 && (pedidos.map((row, index) => {
+            <ListGroup.Item>Carrinho de compras</ListGroup.Item>        
+            
+              {qtdPedidos > 0 && (pedidos.map(row => {
                 console.log(row)
                 return(
                   <Container>
-                  <ListGroup.Item>Carrinho de compras</ListGroup.Item>        
                     <ListGroup variant="flush">
-
-                      <ListGroup.Item>preço:{row.total_price}</ListGroup.Item>
+                      <ListGroup.Item>preço:{row.total_price} 
+                        <Button onClick={()=>{}}>remover</Button>
+                        </ListGroup.Item>
                     </ListGroup> 
                   </Container>
                 );
               })
               )}
+            { qtdPedidos > 0 ?<ListGroup.Item>Finalizar</ListGroup.Item>: <p>insira algo na lista primeiro </p>}        
             </Col>
           </Row>   
         </Col>
